243de403cc519a21920bcbd5ca00158545efe8fe
Andy <andygnewman@gmail.com>
2015-04-28 14:05:16 +0100

    unit test game for evaluating winner

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,6 +5,9 @@ describe Game do
   let(:game) {Game.new(board_class)}
   let(:board_class) {double :board_class, new: board}
   let(:board) {double :board}
+  let(:comp_winning_board) {{1 => :x, 2 => :a, 3 => :o, 4 => :o, 5 => :x, 6 => :a, 7 => :a, 8 => :a, 9 => :x}}
+  let(:human_winning_board) {{1 => :x, 2 => :a, 3 => :o, 4 => :a, 5 => :x, 6 => :o, 7 => :x, 8 => :a, 9 => :o}}
+  let(:no_winning_board) {{1 => :x, 2 => :a, 3 => :o, 4 => :a, 5 => :x, 6 => :a, 7 => :x, 8 => :a, 9 => :o}}
 
   context 'a game when initialized:' do
 
@@ -23,6 +26,25 @@ describe Game do
     it 'should swap turns between the computer and human' do
       game.swap_turn
       expect(game.turn).to eq(:human)
+    end
+
+    context 'evaluating a winner:' do
+
+      it 'should announce the computer wins with 3 in a row' do
+        allow(game).to receive(:board).and_return(comp_winning_board)
+        expect(game.winner).to eq('Computer Wins')
+      end
+
+      it 'should announce the human wins with 3 in a row - never happen!' do
+        allow(game).to receive(:board).and_return(human_winning_board)
+        expect(game.winner).to eq('Human Wins')
+      end
+
+      it 'should return false if no winner' do
+        allow(game).to receive(:board).and_return(no_winning_board)
+        expect(game.winner).to be(false)
+      end
+
     end
 
   end
